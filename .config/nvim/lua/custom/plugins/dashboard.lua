1a9f8cbc0e08206eddb2575fc984a0522df96ec2
ArthurTimofey <arthur.timofey@gmail.com>
2023-12-29 14:02:27 +0000

    modified dashboard

--- .config/nvim/lua/custom/plugins/dashboard.lua
+++ .config/nvim/lua/custom/plugins/dashboard.lua
@@ -1,10 +1,23 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-      -- config
-    }
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = function()
+    local dashboard = require 'alpha.themes.startify'
+    local logo = [[
+        ██████╗ ██╗███╗   ██╗ ██████╗      ██████╗██╗  ██╗██╗██╗     ██╗     ██╗███╗   ██╗ ██████╗ 
+        ██╔══██╗██║████╗  ██║██╔════╝     ██╔════╝██║  ██║██║██║     ██║     ██║████╗  ██║██╔════╝ 
+        ██████╔╝██║██╔██╗ ██║██║  ███╗    ██║     ███████║██║██║     ██║     ██║██╔██╗ ██║██║  ███╗
+        ██╔══██╗██║██║╚██╗██║██║   ██║    ██║     ██╔══██║██║██║     ██║     ██║██║╚██╗██║██║   ██║
+        ██████╔╝██║██║ ╚████║╚██████╔╝    ╚██████╗██║  ██║██║███████╗███████╗██║██║ ╚████║╚██████╔╝
+        ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝      ╚═════╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+      ]]
+
+    dashboard.section.header.val = vim.split(logo, '\n')
+    dashboard.opts.layout[1].val = 8
+
+    return dashboard
   end,
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  config = function(_, dashboard)
+    require('alpha').setup(dashboard.opts)
+  end,
 }
